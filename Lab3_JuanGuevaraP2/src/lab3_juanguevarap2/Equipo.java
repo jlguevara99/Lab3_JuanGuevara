51bc4983efd64d9d3609f0d5a131fca2c1ab9b1b
Juan Guevara <Juan Guevara>
2017-05-05 16:04:45 -0600

    Nuevos avances en menu

--- Lab3_JuanGuevaraP2/src/lab3_juanguevarap2/Equipo.java
+++ Lab3_JuanGuevaraP2/src/lab3_juanguevarap2/Equipo.java
@@ -15,14 +15,34 @@ public class Equipo {
     public String nombre;
     public int palma;
     public ArrayList<Jugador> lista;
+    public double presupuesto;
+    public String ciudad;
 
     public Equipo() {
     }
 
-    public Equipo(String nombre, int palma, ArrayList<Jugador> lista) {
+    public Equipo(String nombre, int palma,double presupuesto,String ciudad, ArrayList<Jugador> lista) {
         this.nombre = nombre;
         this.palma = palma;
         this.lista = lista;
+        this.presupuesto = presupuesto;
+        this.ciudad = ciudad;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public double getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(double presupuesto) {
+        this.presupuesto = presupuesto;
     }
 
     public String getNombre() {
--- Lab3_JuanGuevaraP2/src/lab3_juanguevarap2/Lab3_JuanGuevaraP2.java
+++ Lab3_JuanGuevaraP2/src/lab3_juanguevarap2/Lab3_JuanGuevaraP2.java
@@ -29,12 +29,13 @@ public class Lab3_JuanGuevaraP2 {
             System.out.println("2. Modificar Jugador");
             System.out.println("3. Eliminar Jugador");
             System.out.println("4. Crear equipo");
-            System.out.println("5. Eliminar equipo");
-            System.out.println("6. Hacer compras ");
-            System.out.println("7. Organizar equipo");
-            System.out.println("8. Listar equipo");
-            System.out.println("9. Listar jugadores");
-            System.out.println("10. Salir");
+            System.out.println("5. Modificar equipo");
+            System.out.println("6. Eliminar equipo");
+            System.out.println("7. Hacer compras ");
+            System.out.println("8. Organizar equipo");
+            System.out.println("9. Listar equipo");
+            System.out.println("10. Listar jugadores");
+            System.out.println("11. Salir");
             opcion = sc.nextInt();
             switch (opcion) {
                 case 1:
@@ -173,7 +174,7 @@ public class Lab3_JuanGuevaraP2 {
                             System.out.println("19. juego con los pies");
                         }
                         int modificar = sc.nextInt();
-                        while (modificar < 1 || modificar > 6) {
+                        while (modificar < 1 || modificar > 19) {
                             System.out.println("posicion incorrecta");
                             System.out.println("Ingresela de nuevo");
                             modificar = sc.nextInt();
@@ -188,7 +189,7 @@ public class Lab3_JuanGuevaraP2 {
                                 System.out.println("Nuevo Apellido: ");
                                 String apellido = sc.next();
                                 jugadores.get(indice).setApellido(apellido);
-                                
+
                                 break;
                             case 3:
                                 System.out.println("Nueva edad: ");
@@ -219,66 +220,211 @@ public class Lab3_JuanGuevaraP2 {
                             case 8:
                                 System.out.println("Nueva altura: ");
                                 double altura = sc.nextDouble();
-                                ((Delantero)jugadores.get(indice)).setAltura(altura);
+                                ((Delantero) jugadores.get(indice)).setAltura(altura);
                                 break;
                             case 7:
                                 System.out.println("Nuevo nivel de definicion");
                                 int nivel = sc.nextInt();
-                                ((Delantero)jugadores.get(indice)).setDefinicion(nivel);
+                                ((Delantero) jugadores.get(indice)).setDefinicion(nivel);
                                 break;
                             case 9:
                                 System.out.println("nueva velocidad: ");
                                 double velocidad = sc.nextDouble();
-                                ((Delantero)jugadores.get(indice)).setVelocidad(velocidad);
+                                ((Delantero) jugadores.get(indice)).setVelocidad(velocidad);
                                 break;
                             case 10:
                                 System.out.println("Nuevo promedio de goles: ");
                                 double promedio = sc.nextDouble();
-                                ((Delantero)jugadores.get(indice)).setPromedio(promedio);
+                                ((Delantero) jugadores.get(indice)).setPromedio(promedio);
                                 break;
                             case 11:
                                 System.out.println("Nivel de creatividad: ");
                                 int creatividad = sc.nextInt();
-                                ((Medio)jugadores.get(indice)).setCreatividad(creatividad);
+                                ((Medio) jugadores.get(indice)).setCreatividad(creatividad);
                                 break;
                             case 12:
                                 System.out.println("Nivel de dominio:");
                                 creatividad = sc.nextInt();
-                                ((Medio)jugadores.get(indice)).setCreatividad(creatividad);
+                                ((Medio) jugadores.get(indice)).setCreatividad(creatividad);
                                 break;
                             case 13:
                                 System.out.println("Asistencias: ");
                                 double asistencias = sc.nextDouble();
-                                ((Medio)jugadores.get(indice)).setAsistencias(asistencias);
+                                ((Medio) jugadores.get(indice)).setAsistencias(asistencias);
                                 break;
                             case 14:
                                 System.out.println("nueva agresividad: ");
                                 creatividad = sc.nextInt();
-                                ((Defensa)jugadores.get(indice)).setAgresividad(creatividad);
+                                ((Defensa) jugadores.get(indice)).setAgresividad(creatividad);
                                 break;
                             case 15:
                                 System.out.println("Nueva altura: ");
                                 altura = sc.nextInt();
-                                ((Defensa)jugadores.get(indice)).setAltura(altura);
+                                ((Defensa) jugadores.get(indice)).setAltura(altura);
                                 break;
                             case 16:
                                 System.out.println("Nuevo peso: ");
-                                 double peso = sc.nextInt();
+                                double peso = sc.nextInt();
+                                ((Defensa) jugadores.get(indice)).setPeso(peso);
+                                break;
+                            case 17:
+                                System.out.println("Nueva velocidad: ");
+                                velocidad = sc.nextDouble();
+                                ((Defensa) jugadores.get(indice)).setVelocidad(velocidad);
+                                break;
+                            case 18:
+                                System.out.println("Nuevo juego aereo: ");
+                                int aereo = sc.nextInt();
+                                ((Portero) jugadores.get(indice)).setAereo(aereo);
+                                break;
+                            case 19:
+                                System.out.println("Nuevo juego con los pies: ");
+                                int pies = sc.nextInt();
+                                ((Portero) jugadores.get(indice)).setPies(pies);
+                                break;
                         }
                         System.out.println("Desea seguir modificando)[1 si ; 2 no]");
                         seguir = sc.nextInt();
                     } while (seguir != 2);
                     break;
                 case 3:
-                    System.out.println("ELIMINAR JUGADOR");
-                    System.out.println("Ingrese posicion del jugador que desea eliminar");
-                    int indice = sc.nextInt();
-                    while(){
-                        
-                    }
+                    int op = 0;
+                    do {
+                        System.out.println("ELIMINAR JUGADOR");
+                        for (int i = 0; i < jugadores.size(); i++) {
+                            System.out.println(i + ". " + jugadores.get(i));
+                        }
+                        System.out.println("Ingrese posicion del jugador que desea eliminar");
+                        int indice = sc.nextInt();
+                        while (indice < 1 || indice >= jugadores.size()) {
+                            System.out.println("INCORRECTO");
+                            System.out.println("Ingrese posicion del jugador que desea eliminar");
+                            indice = sc.nextInt();
+                        }
+                        jugadores.remove(indice);
+                        System.out.println("Desea seguir eliminando?[1/2]");
+                        op = sc.nextInt();
+                    } while (op != 2);
+                    break;
+                case 4:
+                    int continuar = 0;
+                    do {
+                        System.out.println("EQUIPOS");
+                        System.out.println("Ingrese nombre del equipo: ");
+                        sc.nextLine();
+                        String nombre = sc.nextLine();
+                        System.out.println("Ingrese palmares: ");
+                        int palmares = sc.nextInt();
+                        System.out.println("Ingrese la ciudad: ");
+                        sc.nextLine();
+                        String ciudad = sc.nextLine();
+                        System.out.println("Ingrese el presupuesto: ");
+                        double presupuesto = sc.nextDouble();
+
+                        equipos.add(new Equipo(nombre, palmares, presupuesto, ciudad, new ArrayList()));
+                    } while (continuar != 2);
+                    break;
+                case 5:
+                    continuar = 0;
+                    do {
+                        System.out.println("ELIMINAR EQUIPOS");
+                        System.out.println("Ingrese la posicion");
+                        int pos = sc.nextInt();
+                        while (pos < 1 || pos >= equipos.size()) {
+                            System.out.println("INCORRECTO");
+                            System.out.println("Ingrese posicion del EQUIPO que desea eliminar");
+                            pos = sc.nextInt();
+                        }
+                        equipos.remove(pos);
+                        System.out.println("Desea continuar?[1/2]");
+                        continuar = sc.nextInt();
+                    } while (continuar != 2);
+                    break;
+                case 6:
+                    continuar = 0;
+                    do {
+                        int pos = 0;
+                        System.out.println("MODIFICAR EQUIPOS");
+                        System.out.println("Ingrese posicion");
+                        pos = sc.nextInt();
+                        while (pos < 1 || pos >= equipos.size()) {
+                            System.out.println("INCORRECTO");
+                            System.out.println("Ingrese posicion del EQUIPO que desea eliminar");
+                            pos = sc.nextInt();
+                        }
+                        System.out.println("1. Nombre");
+                        System.out.println("2. Palmares");
+                        System.out.println("3. Ciudad");
+                        System.out.println("4. Presupuesto ");
+                        int sub = sc.nextInt();
+                        switch (sub) {
+                            case 1:
+                                System.out.println("Nombre nuevo:");
+                                sc.nextLine();
+                                String nom = sc.nextLine();
+                                equipos.get(pos).setNombre(nom);
+                                break;
+                            case 2:
+                                System.out.println("Palmares: ");
+                                int palm = sc.nextInt();
+                                equipos.get(pos).setPalma(palm);
+                                break;
+                            case 3:
+                                System.out.println("Nueva ciudad: ");
+                                sc.nextLine();
+                                nom = sc.nextLine();
+                                equipos.get(pos).setCiudad(nom);
+                                break;
+                            case 4:
+                                System.out.println("presupuesto: ");
+                                double prep = sc.nextDouble();
+                                equipos.get(pos).setPresupuesto(prep);
+                                break;
+                        }
+                        System.out.println("Desea continuar[1/2]");
+                        continuar = sc.nextInt();
+                    } while (continuar != 2);
+                    break;
+                case 7:
+                    int resp = 0;
+
+                    do {
+
+                        for (int i = 0; i < equipos.size(); i++) {
+                            System.out.println(i + ". " + equipos.get(i));
+                        }
+                        System.out.println("HACER COMPRAS");
+                        System.out.println("Ingrese el equipo");
+                        int equipo = sc.nextInt();
+                        while (equipo < 1 || equipo >= equipos.size()) {
+                            System.out.println("INCORRECTO");
+                            System.out.println("Ingrese posicion del EQUIPO que desea comprar");
+                            equipo = sc.nextInt();
+                        }
+                        System.out.println("Lista de jugadores");
+                        for (int i = 0; i < jugadores.size(); i++) {
+                            if (jugadores.get(i).getEstado() == 1) {
+                                System.out.println(i + ". " + jugadores.get(i));
+                            }
+                        }
+                        System.out.println("Ingrese la posicion del jugador que desea comprar");
+                        int jugador = sc.nextInt();
+                        while (jugador < 1 || jugador >= equipos.size()) {
+                            System.out.println("INCORRECTO");
+                            System.out.println("Ingrese posicion del jugador que desea comprar");
+                            jugador = sc.nextInt();
+                        }
+                        if (equipos.get(equipo).getPresupuesto() >= jugadores.get(jugador).getPrecio()) {
+                            System.out.println("La compra se ha realizado");
+                            double nuevoP = equipos.get(equipo).getPresupuesto() - jugadores.get(jugador).getPrecio();
+                            equipos.get(equipo).setPresupuesto(nuevoP);
+                        }
+                        System.out.println("Desea continuar[1 si/2 no]");
+                        resp = sc.nextInt();
+                    } while (resp != 2);
 
             }
-        } while (opcion != 10);
+        } while (opcion != 11);
 
     }
 
